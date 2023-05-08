78d37792a1728a69344a3ad27575c3b241caa7f0
Amritanshu Sharma <98414882+amrit23-web@users.noreply.github.com>
2023-05-08 21:13:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,51 @@
 import streamlit as st
-import pandas as pd
-import pickle
+import joblib
+import numpy as np
 
-# Load the pickled model
-with open('trained_model.sav', 'rb') as f:
-    loaded_model = pickle.load(f)
+# Load the trained model
+model = joblib.load('trained_model.sav')
 
-# Create a function to make predictions
-def predict_chd(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal):
-    data = [[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]]
-    df = pd.DataFrame(data, columns=['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal'])
-    prediction = loaded_model.predict(df)
-    return prediction
+# Define the web interface
+def main():
+    # Add a title to the app
+    st.title('Heart Disease Prediction')
 
-# Add a title and a short description
-st.title('Heart Disease Prediction')
-st.write('Enter the patient details below:')
+    # Add a form for users to enter input data
+    st.subheader('Input Parameters')
+    age = st.number_input('Age')
+    sex = st.selectbox('Sex', ['Male', 'Female'])
+    cp = st.selectbox('Chest Pain Type', ['Typical Angina', 'Atypical Angina', 'Non-anginal Pain', 'Asymptomatic'])
+    trestbps = st.number_input('Resting Blood Pressure')
+    chol = st.number_input('Cholesterol')
+    fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dl', ['True', 'False'])
+    restecg = st.selectbox('Resting ECG', ['Normal', 'ST-T Abnormality', 'Left Ventricular Hypertrophy'])
+    thalach = st.number_input('Maximum Heart Rate Achieved')
+    exang = st.selectbox('Exercise Induced Angina', ['Yes', 'No'])
+    oldpeak = st.number_input('ST Depression Induced by Exercise')
 
-# Add input fields for patient details
-age = st.number_input('Age', min_value=1, max_value=120)
-sex = st.radio('Sex', options=['Male', 'Female'])
-cp = st.selectbox('Chest pain type', options=[0, 1, 2, 3])
-trestbps = st.number_input('Resting blood pressure (mm Hg)', min_value=1, max_value=300)
-chol = st.number_input('Serum cholesterol (mg/dl)', min_value=1, max_value=1000)
-fbs = st.radio('Fasting blood sugar > 120 mg/dl', options=['Yes', 'No'])
-restecg = st.selectbox('Resting electrocardiographic results', options=[0, 1, 2])
-thalach = st.number_input('Maximum heart rate achieved', min_value=1, max_value=300)
-exang = st.radio('Exercise induced angina', options=['Yes', 'No'])
-oldpeak = st.number_input('ST depression induced by exercise relative to rest', min_value=0.0, max_value=10.0)
-slope = st.selectbox('The slope of the peak exercise ST segment', options=[0, 1, 2])
-ca = st.selectbox('Number of major vessels (0-3) colored by flourosopy', options=[0, 1, 2, 3])
-thal = st.selectbox('Thalassemia', options=[0, 1, 2, 3])
+    # Convert the input data into a numpy array
+    sex_value = 1 if sex == 'Male' else 0
+    cp_value = {
+        'Typical Angina': 0,
+        'Atypical Angina': 1,
+        'Non-anginal Pain': 2,
+        'Asymptomatic': 3
+    }[cp]
+    fbs_value = 1 if fbs == 'True' else 0
+    restecg_value = {
+        'Normal': 0,
+        'ST-T Abnormality': 1,
+        'Left Ventricular Hypertrophy': 2
+    }[restecg]
+    data = np.array([[age, sex_value, cp_value, trestbps, chol, fbs_value, restecg_value, thalach, exang, oldpeak]])
 
-# Call the predict_chd function and display the prediction
-if st.button('Predict'):
-    sex = 1 if sex == 'Male' else 0
-    fbs = 1 if fbs == 'Yes' else 0
-    exang = 1 if exang == 'Yes' else 0
-    prediction = predict_chd(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal)
-    if prediction[0] == 1:
-        st.write('The patient is likely to have heart disease.')
-    else:
-        st.write('The patient is unlikely to have heart disease.')
+    # Add a button to run the model and make a prediction
+    if st.button('Predict'):
+        # Use the model to make a prediction
+        prediction = model.predict(data)[0]
+        # Show the prediction result
+        st.subheader('Prediction Result')
+        st.write('The patient is likely to have heart disease.' if prediction else 'The patient is not likely to have heart disease.')
+
+if __name__ == '__main__':
+    main()
