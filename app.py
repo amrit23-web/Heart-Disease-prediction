25444186b43f33147018a9f94407448bf1372999
Amritanshu Sharma <98414882+amrit23-web@users.noreply.github.com>
2023-06-27 22:09:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
-import streamlit as st
+from flask import Flask, render_template, request
 import joblib
 import numpy as np
+
+app = Flask(__name__)
 
 # Load the trained model
 model = joblib.load('trained_model.sav')
 
-# Define the web interface
-def main():
-    # Add a title to the app
-    st.title('Heart Disease Prediction')
+# Define the route for the home page
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    # Add a form for users to enter input data
-    st.subheader('Input Parameters')
-    age = st.number_input('Age')
-    sex = st.selectbox('Sex', ['Male', 'Female'])
-    cp = st.selectbox('Chest Pain Type', ['Typical Angina', 'Atypical Angina', 'Non-anginal Pain', 'Asymptomatic'])
-    trestbps = st.number_input('Resting Blood Pressure')
-    chol = st.number_input('Cholesterol')
-    fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dl', ['True', 'False'])
-    restecg = st.selectbox('Resting ECG', ['Normal', 'ST-T Abnormality', 'Left Ventricular Hypertrophy'])
-    thalach = st.number_input('Maximum Heart Rate Achieved')
-    exang = st.selectbox('Exercise Induced Angina', ['Yes', 'No'])
-    oldpeak = st.number_input('ST Depression Induced by Exercise')
+# Define the route for the prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Retrieve the input data from the HTML form
+    age = int(request.form['age'])
+    sex = request.form['sex']
+    cp = request.form['cp']
+    trestbps = int(request.form['trestbps'])
+    chol = int(request.form['chol'])
+    fbs = request.form['fbs']
+    restecg = request.form['restecg']
+    thalach = int(request.form['thalach'])
+    exang = request.form['exang']
+    oldpeak = float(request.form['oldpeak'])
 
     # Convert the input data into a numpy array
     sex_value = 1 if sex == 'Male' else 0
@@ -39,13 +43,14 @@ def main():
     }[restecg]
     data = np.array([[age, sex_value, cp_value, trestbps, chol, fbs_value, restecg_value, thalach, exang, oldpeak]])
 
-    # Add a button to run the model and make a prediction
-    if st.button('Predict'):
-        # Use the model to make a prediction
-        prediction = model.predict(data)[0]
-        # Show the prediction result
-        st.subheader('Prediction Result')
-        st.write('The patient is likely to have heart disease.' if prediction else 'The patient is not likely to have heart disease.')
+    # Use the model to make a prediction
+    prediction = model.predict(data)[0]
+
+    # Determine the prediction result
+    result = 'The patient is likely to have heart disease.' if prediction else 'The patient is not likely to have heart disease.'
+
+    # Render the result on a new HTML page
+    return render_template('result.html', prediction_result=result)
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
